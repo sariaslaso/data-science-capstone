fdf1a07b6a2af96364c07a37b2feb2efe7592c13
Susana Arias <sariaslaso@gmail.com>
2022-10-12 16:29:35 -0400

    added link to the shiny app on the slides

--- shiny_app/ui.R
+++ shiny_app/ui.R
@@ -2,7 +2,7 @@ library(shiny)
 
 shinyUI(fluidPage(
     
-    titlePanel("next word"),
+    titlePanel(h1("Next Word")),
     
     fluidRow(
             column(width = 6, offset = 4, 
@@ -10,7 +10,7 @@ shinyUI(fluidPage(
                              value = "Enter text..."),
                    sliderInput("num", h5("number of predictions"), 
                                 value = 1, min = 1, max = 10, ticks = FALSE),
-                   h2("word suggestion"),
+                   h6("word suggestion:"),
                    htmlOutput("predicted_words")
                    )
         )
